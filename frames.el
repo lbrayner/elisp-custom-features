8e910d0ad1061e66d6a6a264fa06440afb6768b2
lbrayner <lbrayner@users.noreply.github.com>
2018-08-04 21:35:20 -0300

    Pimped up *Frames*
    *Frames* windows are highlighted and shown with the name of the previous
    buffer, i.e. the one it currently hides.

--- frames.el
+++ frames.el
@@ -4,50 +4,74 @@
   "Visual representation of frames similar to Vim's :tabs command."
   (interactive)
   (cl-labels
-      ((frame-face (frame text)
+      ((get-previous-buffer (buffer)
+                            (with-current-buffer buffer
+                              (save-selected-window
+                                (select-window (get-buffer-window buffer 0))
+                                (let ((previous (previous-buffer)))
+                                  (next-buffer) previous))))
+       (frame-face (selected-frame frame text)
                    (let* ((selected "orange red")
                           (other "dodger blue")
-                          (color (if (eq frame (selected-frame))
+                          (color (if (eq frame selected-frame)
                                      selected other)))
                      (propertize text 'face `(:foreground ,color))))
-       (show-window (window)
-                    (format "\t%s"
-                            (buffer-name (window-buffer window))))
-       (show-frame (count frame)
-                   (frame-face frame
+       (window-frame (window text)
+                     (let ((name (buffer-name (window-buffer window))))
+                       (if (string= name "*Frames*")
+                           (propertize text 'face '(:foreground "yellow"))
+                         text)))
+       (show-window (other-window-exists-p window)
+                    (let* ((buffer (window-buffer window))
+                           (name (buffer-name buffer))
+                           (window-name
+                            (cond ((not other-window-exists-p) name)
+                                  ((not (string= name "*Frames*")) name)
+                                  (t (buffer-name (get-previous-buffer buffer)))))
+                           (text (format "\t%s" window-name)))
+                      (window-frame window text)))
+       (show-frame (count selected-frame frame)
+                   (frame-face selected-frame frame
                                (format "%d %s"
                                        count
                                        (frame-parameter frame 'name))))
        (represent-windows
-        (representation windows)
+        (representation other-window-exists-p windows)
         (if (not (eq windows nil))
             (let* ((window (car windows))
-                   (shown-window (show-window window)))
+                   (shown-window (show-window other-window-exists-p window)))
               (represent-windows
                (if (string= "" representation)
                    shown-window
                  (concat representation "\n" shown-window))
+               other-window-exists-p
                (cdr windows)))
           representation))
        (represent-frames
-        (count representation frames)
+        (count representation other-window-exists-p selected-frame frames)
         (if (not (eq frames nil))
-            (let* ((frame (car frames))
-                   (represented-windows (represent-windows
-                                         "" (window-list frame)))
-                   (represented-frame (concat
-                                       (show-frame count frame)
-                                       "\n" represented-windows)))
-              (represent-frames
-               (1+ count)
-               (if (string= "" representation)
-                   represented-frame
-                 (concat representation "\n" represented-frame))
-               (cdr frames)))
+            (let ((frame (car frames)))
+              (with-selected-frame frame
+                (let* ((represented-windows (represent-windows
+                                             "" other-window-exists-p
+                                             (window-list frame)))
+                       (represented-frame (concat
+                                           (show-frame count selected-frame frame)
+                                           "\n" represented-windows)))
+                  (represent-frames
+                   (1+ count)
+                   (if (string= "" representation)
+                       represented-frame
+                     (concat representation "\n" represented-frame))
+                   other-window-exists-p
+                   selected-frame
+                   (cdr frames)))))
           representation))
        (frames-buffer ()
-                      (let ((buffer (get-buffer-create "*Frames*"))
-                            (represented-frames (represent-frames 1 "" (frame-list))))
+                      (let ((selected-frame (selected-frame))
+                            (other-window-exists-p
+                             (not (eq (selected-window) (next-window))))
+                            (buffer (get-buffer-create "*Frames*")))
                         (display-buffer buffer)
                         (with-current-buffer buffer
                           (save-selected-window
@@ -56,7 +80,9 @@
                                 (frames-mode))
                             (setq buffer-read-only nil)
                             (erase-buffer)
-                            (insert represented-frames)
+                            (insert (represent-frames
+                                     1 "" other-window-exists-p
+                                     selected-frame (frame-list)))
                             (setq buffer-read-only t))))))
     (frames-buffer)))
 
